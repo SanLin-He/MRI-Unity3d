4ce0f2948810c0c135b43b97f3f2fcf9df0726e6
hejunxue <junxue_he@163.com>
2016-12-08 13:50:19 +0800

    inti

---
+++ MRI-Unity3d/Assets/Scripts/Slice2PNG.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slice2PNG : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
